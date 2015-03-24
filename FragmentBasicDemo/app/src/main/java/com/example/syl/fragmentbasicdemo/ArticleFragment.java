05d45c3c55aed5f056602d9cf4e35e194cd7e218
sylcrq <syl272365943@126.com>
2015-03-25 00:34:34 +0800

    +): Add Fragment Basic Demo

---
+++ FragmentBasicDemo/app/src/main/java/com/example/syl/fragmentbasicdemo/ArticleFragment.java
@@ -0,0 +1,36 @@
+package com.example.syl.fragmentbasicdemo;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ArticleFragment extends Fragment {
+
+
+    public ArticleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_article, container, false);
+
+        TextView textView = (TextView)view.findViewById(R.id.article);
+        textView.setText("dd");
+
+        return view;
+    }
+
+
+}
